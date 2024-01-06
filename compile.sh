a7c67cedad99c56d700c26df3397ce0b05d9fdd2
Cromha <enderman.quickly@gmail.com>
2024-01-06 14:13:32 +0100

    bugfix - add sub-modules of fsspec required for downloading game data at start

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-python -m PyInstaller --console --onefile --name Bane-Of-Wargs source/main.py source/battle.py source/check_yaml.py source/colors.py source/map_item.py source/train.py --add-data yamale/VERSION:yamale
+python -m PyInstaller --console --onefile --name Bane-Of-Wargs source/main.py source/battle.py source/check_yaml.py source/colors.py source/map_item.py source/train.py source/logger_sys.py --add-data yamale/VERSION:yamale --collect-submodules fsspec
