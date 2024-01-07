f5c32fddccb6db4d0c7963a37512bf544a2adf11
Cromha <87318892+OcelotWalrus@users.noreply.github.com>
2024-01-07 15:29:04 +0100

    bugfix - tried to fix bug

--- source/logger_sys.py
+++ source/logger_sys.py
@@ -7,14 +7,15 @@ def log_message(message):
     program_dir = str(appdirs.user_config_dir(appname='Bane-Of-Wargs'))
 
     # Create log file
-    os.mkdir(f'{program_dir}')
-    os.mkdir(f'{program_dir}/game')
-    os.mkdir(f'{program_dir}/game/data')
-    os.mkdir(f'{program_dir}/game/schemas')
-    os.mkdir(f'{program_dir}/game/imgs')
-    os.mkdir(f'{program_dir}/logs')
-    os.mkdir(f'{program_dir}/plugins')
-    os.mkdir(f'{program_dir}/saves')
+    if not os.path.exists(f'{program_dir}'):
+        os.mkdir(f'{program_dir}')
+        os.mkdir(f'{program_dir}/game')
+        os.mkdir(f'{program_dir}/game/data')
+        os.mkdir(f'{program_dir}/game/schemas')
+        os.mkdir(f'{program_dir}/game/imgs')
+        os.mkdir(f'{program_dir}/logs')
+        os.mkdir(f'{program_dir}/plugins')
+        os.mkdir(f'{program_dir}/saves')
     with open(f'{program_dir}/logs/{date.today()}.log', 'w') as f:
         f.write("LOG")
     
