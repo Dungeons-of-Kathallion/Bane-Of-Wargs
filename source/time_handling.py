fb98d203570af5b00b97707ec903e7a626799a87
Cromha <87318892+OcelotWalrus@users.noreply.github.com>
2024-02-25 15:01:40 +0100

    progress - game days upgrade (#62)

--- source/time_handling.py
+++ source/time_handling.py
@@ -1,5 +1,7 @@
 import logger_sys
 import colors
+import calendar
+from datetime import datetime, timedelta
 from colorama import Fore, Back, Style, init, deinit
 from colors import *
 
@@ -11,6 +13,58 @@ init()
 COEFFICIENT = .001389
 
 # Handling Functions
+
+
+def addition_to_date(date, addition):
+    # Get month, day and year and then
+    # calculate the addition and return
+    # the value
+    separated_date = date.split('-', 2)
+    month = separated_date[0]
+    day = separated_date[1]
+    year = separated_date[2]
+
+    date = datetime(int(year), int(month), int(day))
+
+    future_date = str(date + timedelta(days=round(addition)))
+    future_date = future_date.split('-', 2)
+
+    new_month = future_date[1]
+    new_day = future_date[2].replace(' 00:00:00', '')
+    new_year = future_date[0]
+
+    future_date = f"{new_month}-{new_day}-{new_year}"
+
+    return future_date
+
+
+def date_prettifier(date):
+    # Get month, day and year and then
+    # prettify them and return the value
+    separated_date = date.split('-', 2)
+    month = calendar.month_name[int(separated_date[0])]
+    day = separated_date[1]
+    year = separated_date[2]
+
+    # some formatting stuff
+
+    if (
+        day == '11' or
+        day == '12' or
+        day == '13'
+    ):
+        day = f"{day}th"
+    elif day.endswith('1'):
+        day = f"{day}st"
+    elif day.endswith('2'):
+        day = f"{day.strip('2')}2nd"  # so spell checks is happy ;)
+    elif day.endswith('3'):
+        day = f"{day}rd"
+    else:
+        day = f"{day}th"
+    formatted_date = f"{day} {month}, year {year}"
+
+    return formatted_date
 
 
 def get_day_time(game_days):
