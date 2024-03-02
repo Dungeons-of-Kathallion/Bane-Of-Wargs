9e9ad8d8e930be6036894550e8e761f88eaf1e64
Cromha <enderman.quickly@gmail.com>
2024-03-02 19:34:14 +0100

    fix[major] - fixed scripts

--- scripts/map_item.py
+++ scripts/map_item.py
@@ -1,14 +1,10 @@
-import colors
 import yaml
 import appdirs
 import time
 import text_handling
 import terminal_handling
-from colorama import Fore, Back, Style, deinit, init
 from colors import *
-
-# initialize colorama
-init()
+from terminal_handling import cout
 
 
 def print_map_xs(player, map, zone, start_time):
@@ -21,25 +17,25 @@ def print_map_xs(player, map, zone, start_time):
     count = 0
     x_to_remove = -4
     y_to_remove = 4
-    print("╔", end="")
+    cout("╔", end="")
     while count <= 7:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╗")
+    cout("╗")
     current_point_list = []
     for i in range(64):
         if x_to_remove == 4:
             x_to_remove = -4
             y_to_remove -= 1
-            print("║")
+            cout("║")
         if x_to_remove == -4:
-            print("║", end="")
+            cout("║", end="")
         current_point = search_point(player["x"] + x_to_remove, player["y"] + y_to_remove, len(map), map)
         if current_point is not None:
             current_point_data = map[f"point{current_point}"]
             to_print_points += [[current_point_data["x"], current_point_data["y"]]]
         if not player_already_printed and current_point == search_point(player["x"], player["y"], len(map), map):
-            print(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
+            cout(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
             player_already_printed = True
         else:
             print_color = COLOR_BLACK + '?'
@@ -48,19 +44,19 @@ def print_map_xs(player, map, zone, start_time):
                     print_color = get_zone_color(zone[map["point" + str(current_point)]["map zone"]]["type"])
             except Exception as error:
                 pass
-            print(print_color + COLOR_RESET_ALL, end="")
+            cout(print_color + COLOR_RESET_ALL, end="")
             current_point_list += [current_point]
 
         x_to_remove += 1
         count += 1
 
-    print("║")
-    print("╚", end="")
+    cout("║")
+    cout("╚", end="")
     count = 0
     while count <= 7:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╝")
+    cout("╝")
 
     map_printing_starting_ending_time = time.time()
     start_time -= map_printing_starting_ending_time - map_printing_starting_time
@@ -76,25 +72,25 @@ def print_map_s(player, map, zone, start_time):
     count = 0
     x_to_remove = -8
     y_to_remove = 8
-    print("╔", end="")
+    cout("╔", end="")
     while count <= 15:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╗")
+    cout("╗")
     current_point_list = []
     for i in range(256):
         if x_to_remove == 8:
             x_to_remove = -8
             y_to_remove -= 1
-            print("║")
+            cout("║")
         if x_to_remove == -8:
-            print("║", end="")
+            cout("║", end="")
         current_point = search_point(player["x"] + x_to_remove, player["y"] + y_to_remove, len(map), map)
         if current_point is not None:
             current_point_data = map[f"point{current_point}"]
             to_print_points += [[current_point_data["x"], current_point_data["y"]]]
         if not player_already_printed and current_point == search_point(player["x"], player["y"], len(map), map):
-            print(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
+            cout(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
             player_already_printed = True
         else:
             print_color = COLOR_BLACK + '?'
@@ -103,19 +99,19 @@ def print_map_s(player, map, zone, start_time):
                     print_color = get_zone_color(zone[map["point" + str(current_point)]["map zone"]]["type"])
             except Exception as error:
                 pass
-            print(print_color + COLOR_RESET_ALL, end="")
+            cout(print_color + COLOR_RESET_ALL, end="")
             current_point_list += [current_point]
 
         x_to_remove += 1
         count += 1
 
-    print("║")
-    print("╚", end="")
+    cout("║")
+    cout("╚", end="")
     count = 0
     while count <= 15:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╝")
+    cout("╝")
 
     map_printing_starting_ending_time = time.time()
     start_time -= map_printing_starting_ending_time - map_printing_starting_time
@@ -131,25 +127,25 @@ def print_map_m(player, map, zone, start_time):
     count = 0
     x_to_remove = -16
     y_to_remove = 16
-    print("╔", end="")
+    cout("╔", end="")
     while count <= 31:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╗")
+    cout("╗")
     current_point_list = []
     for i in range(1024):
         if x_to_remove == 16:
             x_to_remove = -16
             y_to_remove -= 1
-            print("║")
+            cout("║")
         if x_to_remove == -16:
-            print("║", end="")
+            cout("║", end="")
         current_point = search_point(player["x"] + x_to_remove, player["y"] + y_to_remove, len(map), map)
         if current_point is not None:
             current_point_data = map[f"point{current_point}"]
             to_print_points += [[current_point_data["x"], current_point_data["y"]]]
         if not player_already_printed and current_point == search_point(player["x"], player["y"], len(map), map):
-            print(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
+            cout(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
             player_already_printed = True
         else:
             print_color = COLOR_BLACK + '?'
@@ -158,19 +154,19 @@ def print_map_m(player, map, zone, start_time):
                     print_color = get_zone_color(zone[map["point" + str(current_point)]["map zone"]]["type"])
             except Exception as error:
                 pass
-            print(print_color + COLOR_RESET_ALL, end="")
+            cout(print_color + COLOR_RESET_ALL, end="")
             current_point_list += [current_point]
 
         x_to_remove += 1
         count += 1
 
-    print("║")
-    print("╚", end="")
+    cout("║")
+    cout("╚", end="")
     count = 0
     while count <= 31:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╝")
+    cout("╝")
 
     map_printing_starting_ending_time = time.time()
     start_time -= map_printing_starting_ending_time - map_printing_starting_time
@@ -186,25 +182,25 @@ def print_map_l(player, map, zone, start_time):
     count = 0
     x_to_remove = -32
     y_to_remove = 32
-    print("╔", end="")
+    cout("╔", end="")
     while count <= 63:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╗")
+    cout("╗")
     current_point_list = []
     for i in range(4096):
         if x_to_remove == 32:
             x_to_remove = -32
             y_to_remove -= 1
-            print("║")
+            cout("║")
         if x_to_remove == -32:
-            print("║", end="")
+            cout("║", end="")
         current_point = search_point(player["x"] + x_to_remove, player["y"] + y_to_remove, len(map), map)
         if current_point is not None:
             current_point_data = map[f"point{current_point}"]
             to_print_points += [[current_point_data["x"], current_point_data["y"]]]
         if not player_already_printed and current_point == search_point(player["x"], player["y"], len(map), map):
-            print(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
+            cout(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
             player_already_printed = True
         else:
             print_color = COLOR_BLACK + '?'
@@ -213,19 +209,19 @@ def print_map_l(player, map, zone, start_time):
                     print_color = get_zone_color(zone[map["point" + str(current_point)]["map zone"]]["type"])
             except Exception as error:
                 pass
-            print(print_color + COLOR_RESET_ALL, end="")
+            cout(print_color + COLOR_RESET_ALL, end="")
             current_point_list += [current_point]
 
         x_to_remove += 1
         count += 1
 
-    print("║")
-    print("╚", end="")
+    cout("║")
+    cout("╚", end="")
     count = 0
     while count <= 63:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╝")
+    cout("╝")
 
     map_printing_starting_ending_time = time.time()
     start_time -= map_printing_starting_ending_time - map_printing_starting_time
@@ -239,11 +235,11 @@ def print_full_map(player, map, zone, start_time):
     map_points = list(map)
     map_points_num = len(map_points)
     count = 0
-    print("╔", end="")
+    cout("╔", end="")
     while count <= 128:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╗")
+    cout("╗")
     coord_x = -64
     coord_y = 64
     current_point_list = []
@@ -252,11 +248,11 @@ def print_full_map(player, map, zone, start_time):
         if coord_x > 64:
             coord_x = -64
             coord_y -= 1
-            print("║")
+            cout("║")
         if coord_x < -63:
-            print("║", end="")
+            cout("║", end="")
         if not player_already_printed and coord_x == player_x and coord_y == player_y:
-            print(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
+            cout(COLOR_CYAN + COLOR_STYLE_BRIGHT + "¶" + COLOR_RESET_ALL, end="")
             player_already_printed = True
         else:
             print_color = COLOR_BLACK + '░'
@@ -266,18 +262,18 @@ def print_full_map(player, map, zone, start_time):
                 get_zone = False
             if get_zone and current_point not in current_point_list:
                 print_color = get_zone_color(zone[map["point" + str(current_point)]["map zone"]]["type"])
-            print(print_color + COLOR_RESET_ALL, end="")
+            cout(print_color + COLOR_RESET_ALL, end="")
             if get_zone:
                 current_point_list += [current_point]
         coord_x += 1
         counting += 1
-    print("║")
-    print("╚", end="")
+    cout("║")
+    cout("╚", end="")
     count = 0
     while count <= 128:
-        print("═", end="")
+        cout("═", end="")
         count += 1
-    print("╝")
+    cout("╝")
     map_printing_starting_ending_time = time.time()
     start_time -= map_printing_starting_ending_time - map_printing_starting_time
 
@@ -363,8 +359,8 @@ def get_zone_color(zone_type):
             elif zone_code == 35:
                 zone_color = COLOR_GREENS_0 + '#'
     except Exception as error:
-        print(COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT + f"zone type '{zone_type}' is not a valid zone type." + COLOR_RESET_ALL)
-        print(error)
+        cout(COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT + f"zone type '{zone_type}' is not a valid zone type." + COLOR_RESET_ALL)
+        cout(error)
         text_handling.exit_game()
     return zone_color
 
@@ -386,7 +382,7 @@ def search_point(x, y, map_points_num, map):
 def run(player, map, zone, start_time):
     choices = ['8x8', '16x16', '32x32', '64x64', 'full (128x128)']
     choice = terminal_handling.show_menu(choices, length=19)
-    print("")
+    cout("")
     if choice == choices[0]:
         print_map_xs(player, map, zone, start_time)
     if choice == choices[1]:
@@ -401,6 +397,3 @@ def run(player, map, zone, start_time):
 
 # run the script
 run(player, map, zone, start_time)
-
-# deinitialize colorama
-deinit()
--- scripts/teleportation_rock.py
+++ scripts/teleportation_rock.py
@@ -1,30 +1,31 @@
 import time
 import text_handling
+from terminal_handling import cout, cinput
 
 def teleportation_action(player, map):
-    print(" ")
+    cout(" ")
     text_handling.print_separator("=")
     try:
-        which_coordinates_x = int(input("Where would you like to be at x?\n"))
-        which_coordinates_y = int(input("Where would you like to be at y?\n"))
+        which_coordinates_x = int(cinput("Where would you like to be at x?\n"))
+        which_coordinates_y = int(cinput("Where would you like to be at y?\n"))
         # Here we consider that the player will enter a correct input`, because this is an example
-    
+
         # Check if a map point exists at player wish coordinates
         point_exists = False
         for i in list(map):
             if map[i]["x"] == which_coordinates_x and map[i]["y"] == which_coordinates_y:
                 point_exists = True
-    
+
         # Run the teleportation action
         if point_exists:
-            print("Teleporting...")
+            cout("Teleporting...")
             time.sleep(5)
             player["x"], player["y"] = which_coordinates_x, which_coordinates_y
-            print("Teleported!")
+            cout("Teleported!")
         else:
-            print(f"Where you want to go does not exists...")
+            cout(f"Where you want to go does not exists...")
     except Exception as error:
-        print("Please enter integers as coordinates")
+        cout("Please enter integers as coordinates")
     text_handling.print_separator("=")
 
 # Actually run the action, and tells the game which arguments to use
