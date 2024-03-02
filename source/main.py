c6395dc5a1ea3f1f1c3e87171f4496e55845a302
Cromha <enderman.quickly@gmail.com>
2024-03-02 14:15:56 +0100

    update - updated the game source code version

--- source/main.py
+++ source/main.py
@@ -155,7 +155,7 @@ with open(program_dir + '/preferences.yaml', 'r') as f:
 logger_sys.log_message("INFO: Checking if game source code is up to date")
 global latest_version
 latest_version = None  # placeholder
-SOURCE_CODE_VERSION = 0.13
+SOURCE_CODE_VERSION = 0.135
 latest_main_class = io.StringIO(data_handling.temporary_git_file_download(
     'source/main.py', 'https://github.com/Dungeons-of-Kathallion/Bane-Of-Wargs.git'
 )).readlines()
