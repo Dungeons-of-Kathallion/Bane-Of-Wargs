1269f792ecc10b9ae6d231a1ca60678c0eb4524e
Cromha <enderman.quickly@gmail.com>
2024-03-08 19:43:46 +0100

    progress[major] - added logging level

--- source/logger_sys.py
+++ source/logger_sys.py
@@ -1,10 +1,40 @@
 # external imports
 import appdirs
+import yaml
 from datetime import *
 
 
-def log_message(message):
-    program_dir = str(appdirs.user_config_dir(appname='Bane-Of-Wargs'))
+program_dir = str(appdirs.user_config_dir(appname='Bane-Of-Wargs'))
 
+
+# Handling functions
+
+
+def write_message(message):
     with open(f'{program_dir}/logs/{date.today()}.log', 'a') as f:
         f.write(f'{datetime.now()}: {message}\n')
+
+
+def log_message(message):
+    # Open the user's preferences and get the logging
+    # level. Then, determines the message logging level
+    # and check if the message should be logged.
+    with open(f'{program_dir}/preferences.yaml', 'r') as f:
+        preferences = yaml.safe_load(f)
+
+    info = "INFO: " in message
+    debug = "DEBUG: " in message
+    warning = "WARNING: " in message
+    error = (
+        "ERROR: " in message or
+        "CRITICAL ERROR: " in message or
+        "FATAL ERROR: " in message
+    )
+    if preferences["logging level"] == 1:
+        write_message(message)
+    elif preferences["logging level"] == 2 and not info:
+        write_message(message)
+    elif preferences["logging level"] == 3 and error:
+        write_message(message)
+    else:
+        return False
--- source/main.py
+++ source/main.py
@@ -103,7 +103,7 @@ menu = True
 program_dir = str(appdirs.user_config_dir(appname='Bane-Of-Wargs'))
 first_start = False
 if not os.path.exists(program_dir):
-    GAME_DATA_VERSION = 0.16
+    GAME_DATA_VERSION = 0.165
     os.mkdir(program_dir)
     # Open default config file and store the text into
     # a variable to write it into the user config file
@@ -117,6 +117,7 @@ if not os.path.exists(program_dir):
         "theme": 'greenblue',
         "title style": 1,
         "auto update": False,
+        "logging level": 2,
         "game data download": {
             "branch": "master",
             "repository": "Bane-Of-Wargs",
@@ -155,7 +156,7 @@ with open(program_dir + '/preferences.yaml', 'r') as f:
 logger_sys.log_message("INFO: Checking if game source code is up to date")
 global latest_version
 latest_version = None  # placeholder
-SOURCE_CODE_VERSION = 0.16
+SOURCE_CODE_VERSION = 0.165
 latest_main_class = io.StringIO(data_handling.temporary_git_file_download(
     'source/main.py', 'https://github.com/Dungeons-of-Kathallion/Bane-Of-Wargs.git'
 )).readlines()
