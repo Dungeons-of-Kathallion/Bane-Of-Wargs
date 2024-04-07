3c1f37114ebcf258f8e0258283918d3571289b36
Cromha <enderman.quickly@gmail.com>
2024-04-07 11:11:47 +0200

    tweak - tweaked messages

--- source/yaml_handling.py
+++ source/yaml_handling.py
@@ -17,7 +17,11 @@ def safe_load(file, crash=False) -> dict:
     try:
         output = yaml.safe_load(file)
     except Exception as debug:
-        cout(COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT + " failed to parse yaml file!")
+        cout(
+            COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT +
+            " failed to parse yaml file! Check logs for further information." +
+            COLOR_RESET_ALL
+        )
         logger_sys.log_message(f"ERROR: Failed to parse yaml file '{file}'")
         logger_sys.log_message(f"DEBUG: {debug}")
         time.sleep(2)
@@ -31,7 +35,11 @@ def dump(data, crash=False) -> str:
     try:
         output = yaml.dump(data)
     except Exception as debug:
-        cout(COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT + " failed to dump yaml data!")
+        cout(
+            COLOR_RED + "ERROR: " + COLOR_STYLE_BRIGHT +
+            " failed to dump data! Check logs for further information." +
+            COLOR_RESET_ALL
+        )
         logger_sys.log_message(f"ERROR: Failed to dump data '{dict(itertools.islice(data.items(), 7))}...'")
         logger_sys.log_message(f"DEBUG: {debug}")
         time.sleep(2)
