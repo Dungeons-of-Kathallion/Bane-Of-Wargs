5779fce0b0b8a346b740847de032400159d6eebb
OcelotWalrus <enderman.quickly@gmail.com>
2024-12-29 15:28:02 +0100

    feature[minor] - now display value of items in `I` command

--- source/main.py
+++ source/main.py
@@ -3209,6 +3209,7 @@ def run(play):
                 else:
                     cout("NAME: " + which_item)
                 cout("TYPE: " + item[which_item]["type"])
+                cout("ESTIMATED VALUE: " + str(round(item[which_item]["gold"])))
                 text = "DESCRIPTION: " + item[which_item]["description"]
                 text_handling.print_long_string(text)
                 if (
