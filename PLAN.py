ac8199f4017edd27aec34cc927a9956fade92af4
OcelotWalrus <enderman.quickly@gmail.com>
2025-01-03 13:23:40 +0100

    crap

---
+++ PLAN.py
@@ -0,0 +1,40 @@
+##     -- DIFFERENT REGIONS --
+##
+##   -STALL ISLAND//
+##
+## FORLINDON;
+## -X falchion (found on map point x:-62,y:63)
+##
+## CALIFORN;
+## -0 arming blade (ordered at new hyru's blacksmith)
+## -0 californ xiphos (ordered at new hyru's blacksmith)
+##
+## STALL FIELDS;
+## -X cutlass (ordered at jame's blacksmith)
+##
+## GORO MOUNTS;
+## -X dwarf saber (bought at humil dwarf stable)
+## -X dwarf axe (bought at humil dwarf stable)
+## -X throwing axe (bought at humil dwarf stable)
+## -X dwarf silver chestplate (bought at humil dwarf stable)
+##
+## WASTED MANGROVE LANDS;
+##
+## GERUD;
+## -X gerud saber (ordered at los gerudos armorer)
+## -X pillager falcata (drops from south pillagers)
+## -X pillager claymore (drops from south pillagers)
+## -X cutlass (ordered at los gerudos armorer)
+##
+##   -SHIRE ISLAND//
+##
+## THE SHIRE;
+## -0 shire xiphos (ordered at astrali blacksmith)
+## -0 shire tachi (ordered at astrali blacksmith)
+##
+## ELF LANDS;
+## -X eldin saber (ordered at eldin blacksmith)
+## -0 eldin scimitar (ordered at eldin blacksmith)
+##
+## KIRGON;
+## -0 lyzalfos bracer (drops from lyzalfi)
